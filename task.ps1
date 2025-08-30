9698023efb3f01fa9531395e1e3aeefd06b8d08d
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-30 22:59:15 +0300

    added task.ps1 script for task

--- task.ps1
+++ task.ps1
@@ -15,16 +15,51 @@ Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 
 Write-Host "Creating web network security group..."
-# Write your code for creation of Web NSG here -> 
+$webRule = New-AzNetworkSecurityRuleConfig `
+            -Name web-allow-http-https-rule `
+            -Description "Allow HTTP and HTTPS" `
+            -Access Allow `
+            -Protocol Tcp `
+            -Direction Inbound `
+            -Priority 100 `
+            -SourceAddressPrefix Internet `
+            -SourcePortRange * `
+            -DestinationAddressPrefix * `
+            -DestinationPortRange 80,443
+
+$webSubnetNsg = New-AzNetworkSecurityGroup `
+                -ResourceGroupName $resourceGroupName `
+                -Location $location `
+                -Name $webSubnetName `
+                -SecurityRules $webRule
 
 Write-Host "Creating mngSubnet network security group..."
-# Write your code for creation of management NSG here -> 
+$mngRule = New-AzNetworkSecurityRuleConfig `
+            -Name mng-allow-ssh-rule `
+            -Description "Allow SSH" `
+            -Access Allow `
+            -Protocol Tcp `
+            -Direction Inbound `
+            -Priority 100 `
+            -SourceAddressPrefix Internet `
+            -SourcePortRange * `
+            -DestinationAddressPrefix * `
+            -DestinationPortRange 22
+
+$mngSubnetNsg = New-AzNetworkSecurityGroup `
+                -ResourceGroupName $resourceGroupName `
+                -Location $location `
+                -Name $mngSubnetName `
+                -SecurityRules $mngRule
 
 Write-Host "Creating dbSubnet network security group..."
-# Write your code for creation of management NSG here -> 
+$dbSubnetNsg = New-AzNetworkSecurityGroup `
+                -ResourceGroupName $resourceGroupName `
+                -Location $location `
+                -Name $dbSubnetName `
 
 Write-Host "Creating a virtual network ..."
-$webSubnet = New-AzVirtualNetworkSubnetConfig -Name $webSubnetName -AddressPrefix $webSubnetIpRange
-$dbSubnet = New-AzVirtualNetworkSubnetConfig -Name $dbSubnetName -AddressPrefix $dbSubnetIpRange
-$mngSubnet = New-AzVirtualNetworkSubnetConfig -Name $mngSubnetName -AddressPrefix $mngSubnetIpRange
+$webSubnet = New-AzVirtualNetworkSubnetConfig -Name $webSubnetName -AddressPrefix $webSubnetIpRange -NetworkSecurityGroup $webSubnetNsg
+$dbSubnet = New-AzVirtualNetworkSubnetConfig -Name $dbSubnetName -AddressPrefix $dbSubnetIpRange -NetworkSecurityGroup $dbSubnetNsg
+$mngSubnet = New-AzVirtualNetworkSubnetConfig -Name $mngSubnetName -AddressPrefix $mngSubnetIpRange -NetworkSecurityGroup $mngSubnetNsg
 New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $webSubnet,$dbSubnet,$mngSubnet
